732f6d473ad3f29fbaf2cc594b5b3236a355b0a4
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-02-07 08:05:59 +0100

    Time: 47 ms (18.32%) | Memory: 16.8 MB (51.34%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,21 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        cont=True
-        prefix = ""
-        shortest= min(strs,key=len) #finds shortest word in the list
+        #find the shortest string in the list
+        shortes_string = min(strs,key=len)
+        common_prefix = ""
+        #parcourir le mot car par car 
+        for car in shortes_string:
+            common_prefix = common_prefix + car
+            #pour chaque pref si il est prefix de tout les mots de la liste
+            for word in strs :
+                if not word.startswith(common_prefix):
+                    #retourner le prefixe deja exisitant
+                        return common_prefix[:-1]
 
-        for char in shortest : #flow
-            #add new char to prefix
-            prefix += char #f
-            
-            for s in strs:
-                if s[:len(prefix)] != prefix  :
-                    cont=False
-                    break; #no need to persue the search in the rest of the strings
-            if not cont:
-                prefix = prefix[:-1]
-                break;    
-                
-
-        return prefix
+        return shortes_string
         
