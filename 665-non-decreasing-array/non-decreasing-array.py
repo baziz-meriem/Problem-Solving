32484bfd586fbaf897b180abecf17584967d4cf1
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-02-10 09:23:10 +0100

    Time: 153 ms (58.11%) | Memory: 18 MB (58.57%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.py
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -8,20 +8,25 @@ class Solution:
         for idx in range(len(nums) - 1):
             if nums[idx] > nums[idx + 1]:
                 violations += 1
-                viol_idx = idx + 1
+                viol_idx = idx
                 if violations > 1:
                     return False
 
         if violations == 0:
             return True
 
-        # Attempt to fix the violation
-        if viol_idx == 1 or nums[viol_idx - 2] <= nums[viol_idx]:
-            nums[viol_idx - 1] = nums[viol_idx]
-        elif viol_idx == len(nums) - 1 or nums[viol_idx - 1] <= nums[viol_idx + 1]:
-            nums[viol_idx] = nums[viol_idx - 1]
-        else:
-            return False
+        if violations == 1:
+            if viol_idx==0 :
+                # Attempt to fix the violation
+                nums[viol_idx] = nums[viol_idx+1]
+                
+            else :
+                if viol_idx+2<len(nums) and nums[viol_idx+2]< nums[viol_idx]:
+                    nums[viol_idx] = nums[viol_idx+1]
+                else:
+                    nums[viol_idx+1] = nums[viol_idx]
+                
+
 
         # Check if the array is non-decreasing after modification
         for idx in range(len(nums) - 1):
