8f9deaac616ac92cdc9b9baec0575b98fc199eba
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-02-13 07:59:34 +0100

    Time: 155 ms (82.09%) | Memory: 18.3 MB (51.11%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -8,9 +8,8 @@ class Solution:
         while idx+2 < len(nums):
             all_sides = nums[idx:idx+3]
            
-            if sum(all_sides[:-1]) > all_sides[-1] and sum(all_sides[-2:]) > all_sides[0] and all_sides[-1]+all_sides[0] > all_sides[1]:
-                if sum(all_sides) > max_sum:
-                    max_sum = sum(all_sides)
+            if sum(all_sides[-2:]) > all_sides[0] :
+                return sum(all_sides)
                 
             idx += 1
         return max_sum
