ac17382268077fff631c219abfff617d95c7b108
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-02-22 11:32:51 +0100

    Time: 46 ms (82.48%) | Memory: 16.9 MB (55.53%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,26 +1,27 @@
-from typing import List
-
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
         new_nums = []
         zeros = 0
-        
-        for idx in range(len(nums) - 1):  # Iterate up to len(nums) - 1
+        upper_range = len(nums)-1
+  
+        for idx in range(0,upper_range) :
+
             if nums[idx] != 0:
-                if nums[idx] == nums[idx + 1]:
-                    nums[idx] = nums[idx + 1] * 2
-                    nums[idx + 1] = 0
+
+                if nums[idx] == nums[idx+1]:
+                    nums[idx] = nums[idx+1] *2
+                    nums[idx+1] = 0
+
                 new_nums.append(nums[idx])
             else:
-                zeros += 1
+                zeros +=1
                 
-        # Handle the last element properly
-        if nums[-1] != 0:
-            new_nums.append(nums[-1])
-        else:
-            zeros += 1
-            
-        # Append zeros to new_nums
-        new_nums.extend([0] * zeros)
+        new_nums.append(nums[idx+1])
+                    
+
+        new_nums.extend([0]*zeros)
 
         return new_nums
+
+
+        
