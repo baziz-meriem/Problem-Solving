00a2b2568076b7d4115f80cdb3ead18d8ae8863a
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-03-04 09:38:55 +0100

    Time: 40 ms (24.43%) | Memory: 16.5 MB (96.31%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -1,25 +1,13 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
-        str_num= str(num)
-        negative=False
-        if str_num[0]=='-':
-            negative=True
-            str_num = str_num[1:]
-
-        nums_list = sorted([int(num) for num in list(str_num)])
-
-        if nums_list[0] == 0 and negative==False and len(nums_list)>1:
-            idx=1
-            while nums_list[idx]==0 and idx < len(nums_list)-1:
-                idx += 1
-            nums_list[idx],nums_list[0] =  nums_list[0],nums_list[idx]
-
-        nums_list_str = [ str(num) for num in nums_list ] 
-
-        if negative:
-            out = int('-'+ ''.join(nums_list_str[::-1]))
+        str_num = str(num)
+        if str_num[0] == '-':
+            nums_list = sorted(str_num[1:])
+            return int('-' + ''.join(nums_list[::-1]))
         else:
-            out = int(''.join(nums_list_str))
-
-        return out
-        
+            nums_list = sorted(str_num)
+            if nums_list[0] == '0' and len(nums_list) > 1:
+                non_zero_idx = next((i for i, n in enumerate(nums_list) if n != '0'), None)
+                if non_zero_idx:
+                    nums_list[0], nums_list[non_zero_idx] = nums_list[non_zero_idx], nums_list[0]
+            return int(''.join(nums_list))
