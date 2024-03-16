8ff19d7e17d2b9313835bed85955fecf72a20a92
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-03-16 11:10:51 +0100

    Time: 1136 ms (5.38%) | Memory: 59 MB (5.91%) - LeetSync

--- 3170-find-indices-with-index-and-value-difference-ii/find-indices-with-index-and-value-difference-ii.py
+++ 3170-find-indices-with-index-and-value-difference-ii/find-indices-with-index-and-value-difference-ii.py
@@ -1,22 +1,33 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        n = len(nums)
-        S = SortedList()
 
-        for i in range(n):
-            if i >= indexDifference:
-                x = i - indexDifference
-                y = nums[x]
-                S.add((y, x))
+        min_max_dict ={}
+        min_max_dict[len(nums)-1] = [   [nums[len(nums)-1],len(nums)-1],#min,idx
+                                        [nums[len(nums)-1],len(nums)-1]#max,idx
+                                        ]
+        for n in range(len(nums)-2,-1,-1):
+            max_val = max(nums[n], min_max_dict[n+1][1][0])
+            max_idx = n if nums[n]>min_max_dict[n+1][1][0] else min_max_dict[n+1][1][1]
 
-            upper_bound = S.bisect_left((nums[i] + valueDifference, 0))
-            if upper_bound != len(S):
-                return [S[upper_bound][1], i]
+            min_val =  min(nums[n], min_max_dict[n+1][0][0])
+            min_idx = n if nums[n]<min_max_dict[n+1][0][0] else min_max_dict[n+1][0][1]
 
-            lower_bound = S.bisect_left((nums[i] - valueDifference + 1, 0))
-            if lower_bound > 0:
-                return [S[lower_bound - 1][1], i]
+            min_max_dict[n] = [[min_val,min_idx],[max_val,max_idx]]
+       
 
-        return [-1, -1]
+        for idx,val in enumerate(nums[:len(nums)-indexDifference]):
+            max_val = min_max_dict[idx+indexDifference][1][0]
+            idx_max = min_max_dict[idx+indexDifference][1][1]
+
+            min_val = min_max_dict[idx+indexDifference][0][0]
+            idx_min = min_max_dict[idx+indexDifference][0][1]
+
+            if abs(max_val-val)>= valueDifference:
+                return [idx,idx_max]
+
+            elif abs(min_val-val)>= valueDifference:
+                return [idx,idx_min]
+            else:
+                continue
+        return [-1,-1]
+        
