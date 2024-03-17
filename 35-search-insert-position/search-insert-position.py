c373a7072fc9f673f2d2df5229893d31135162f5
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-03-17 21:30:48 +0100

    Time: 48 ms (66.53%) | Memory: 17.3 MB (85.85%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,9 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums[-1]< target:
+            return len(nums)
         for idx,val in enumerate(nums):
-            print(idx)
+            
             if val == target:
                 return idx
             elif val > target :
                 return idx
-        return idx+1
+        
