92bb485bb297c6d3f3d07ece2020592c2acbcf17
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-04-10 02:00:33 +0100

    Time: 668 ms (47.55%) | Memory: 30.8 MB (33.95%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,9 +1,19 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-
-        arr = [0]*(n+1)
-        for lv, ar, seats in bookings:
-            arr[lv-1]+= seats
-            arr[ar]-= seats
-
-        return list(accumulate(arr[:-1]))
+        
+        booked_flights=[0]*n
+        flights = []
+        for first,last,seats in bookings:
+            booked_flights[first-1] += seats
+            if last <n:
+                booked_flights[last] -= seats
+       
+        #print(booked_flights)
+        for idx in range(len(booked_flights)):
+            if idx>0:
+                booked_flights[idx] += booked_flights[idx-1]  
+            
+        #print(booked_flights)
+        for seats in booked_flights:
+            flights.append(seats)
+        return flights
