08d17b046ccfbb9006783211e0ffcee88082e979
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-04-10 15:21:26 +0100

    Time: 352 ms (7.70%) | Memory: 29.9 MB (12.91%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,10 +1,17 @@
 class Solution:
-    def maxScore(self, cardPoints: List[int], K: int) -> int:
-        N, S = len(cardPoints), sum(cardPoints)
-        wind = ans = sum(cardPoints[:N-K])
-
-        for right in range(N-K,N):
-            wind = wind - cardPoints[right - N + K] + cardPoints[right]
-            ans = min(ans,wind)
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        window_size = len(cardPoints)-k
         
-        return S - ans
+        total_sum = sum(cardPoints)
+        min_sum = sum(cardPoints[0:window_size])
+        pref_sum=[cardPoints[0]]
+
+        for idx in range(1,len(cardPoints)):
+            pref_sum.append(pref_sum[-1]+cardPoints[idx])
+
+        idx = 1
+        while idx < len(cardPoints)-window_size+1:
+            current_sum = pref_sum[idx+window_size-1] - pref_sum[idx-1]
+            min_sum = min(min_sum,current_sum)
+            idx += 1
+        return total_sum - min_sum
