c86149003d2fcb494b773c964aed7178d4b8d0a7
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-04-12 13:03:33 +0100

    Time: 399 ms (57.45%) | Memory: 26.5 MB (74.73%) - LeetSync

--- 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
@@ -3,22 +3,22 @@ class Solution:
         total = sum(nums)
         n = len(nums)
         copy = target // total
-        extra_target = target % total
+        target %= total  # Use target itself as the remaining part after full cycles
 
         # If after removing full cycles of sum(nums) there is nothing left to find, adjust answer
-        if extra_target == 0:
+        if target == 0:
             return copy * n if copy > 0 else -1
 
-        wind_sum = 0  # Start with no sum, checking for extra_target in the remaining part
+        wind_sum = 0  # Start with no sum, checking for target in the remaining part
         min_wind = float('inf')
         s_wind, e_wind = 0, 0
 
-        # Only need to consider extra_target within 2*n to allow for wrapping
+        # Only need to consider target within 2*n to allow for wrapping
         while e_wind < 2 * n:
             wind_sum += nums[e_wind % n]
 
-            while wind_sum >= extra_target and s_wind <= e_wind:
-                if wind_sum == extra_target:
+            while wind_sum >= target and s_wind <= e_wind:
+                if wind_sum == target:
                     min_wind = min(min_wind, e_wind - s_wind + 1)
                 wind_sum -= nums[s_wind % n]
                 s_wind += 1
