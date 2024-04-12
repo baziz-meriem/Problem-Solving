059d9bfdfebed2cb5a50b080d9826f0516febc46
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-04-12 13:12:46 +0100

    Time: 397 ms (60.04%) | Memory: 26.5 MB (74.73%) - LeetSync

--- 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
@@ -3,17 +3,16 @@ class Solution:
         total = sum(nums)
         n = len(nums)
         copy = target // total
-        target %= total  # Use target itself as the remaining part after full cycles
+        target %= total 
 
-        # If after removing full cycles of sum(nums) there is nothing left to find, adjust answer
+        
         if target == 0:
             return copy * n if copy > 0 else -1
 
-        wind_sum = 0  # Start with no sum, checking for target in the remaining part
+        wind_sum = 0 
         min_wind = float('inf')
         s_wind, e_wind = 0, 0
 
-        # Only need to consider target within 2*n to allow for wrapping
         while e_wind < 2 * n:
             wind_sum += nums[e_wind % n]
 
@@ -25,5 +24,5 @@ class Solution:
 
             e_wind += 1
 
-        # Adjust the result for the cycles skipped
+        
         return (min_wind + copy * n) if min_wind != float('inf') else -1
