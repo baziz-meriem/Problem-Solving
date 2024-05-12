39630e7735e9e28486a1141e367c79c68bcc93eb
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-05-12 23:38:07 +0100

    Time: 39 ms (66.84%) | Memory: 16.9 MB (14.42%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,10 +1,39 @@
+from functools import cache
+
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = nums[:]
 
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+        @cache
+        def dp(i, j, turn):
 
-        return dp[-1] >= 0
+            if j < i: return -math.inf
+
+            if j - i + 1 == 1:
+                return nums[i]
+            
+            if turn == 'p1':
+
+                # take leftmost
+                choice1 = nums[i] + dp(i+1, j, 'p2') 
+                # take rightmost
+                choice2 = nums[j] + dp(i, j-1, 'p2')
+
+                return max(choice1, choice2)
+            
+            elif turn == 'p2':
+
+                # take leftmost
+                choice1 = dp(i+1, j, 'p1') 
+                # take rightmost
+                choice2 = dp(i, j-1, 'p1')
+
+                return min(choice1, choice2)
+
+            else:
+                raise Exception('turn should be p1 or p2')
+
+
+        p1_best = dp(0, len(nums)-1, 'p1')
+        print(p1_best)
+
+        return p1_best >= sum(nums) / 2
