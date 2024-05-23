0f623a4390cb17c5ad6731485ed20f7dda276821
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-05-23 20:39:47 +0100

    Time: 42 ms (61.28%) | Memory: 18.3 MB (12.08%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -1,20 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def widthOfBinaryTree(self, root: TreeNode) -> int:
-        def dfs(node, depth, position):
-            if not node:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(Node,depth,position):
+            if not Node:
                 return
-            
-            if depth not in leftmost_positions:
-                leftmost_positions[depth] = position
-            
-            current_width = position - leftmost_positions[depth] + 1
+            if depth not in leftmost_dict:
+                leftmost_dict[depth] = position
+            current_width = position - leftmost_dict[depth] + 1
             nonlocal max_width
-            max_width = max(max_width, current_width)
-            
-            dfs(node.left, depth + 1, 2 * position)
-            dfs(node.right, depth + 1, 2 * position + 1)
-
-        leftmost_positions = {}
-        max_width = 0
-        dfs(root, 0, 0)
+            max_width = max(max_width,current_width)
+            dfs(Node.left,depth+1,position*2)
+            dfs(Node.right,depth+1,position*2+1)
+        leftmost_dict = {}
+        max_width=0
+        dfs(root,0,0)
         return max_width
