2f1e90c97996564a29ebe8f5d354a999da66ad06
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-21 22:04:48 +0100

    Time: 925 ms (66.65%) | Memory: 22 MB (69.69%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,22 +1,19 @@
+from typing import List
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        total_gas = 0
-        total_cost = 0
         current_gas = 0
         start_index = 0
+        total_gas = 0
         
         for i in range(len(gas)):
-            total_gas += gas[i]
-            total_cost += cost[i]
             current_gas += gas[i] - cost[i]
+            total_gas += gas[i] - cost[i]
             
             # If current gas becomes negative, reset the starting index and current gas
             if current_gas < 0:
                 start_index = i + 1
                 current_gas = 0
         
-        # After the loop, check if we have enough gas to cover the total cost
-        if total_gas >= total_cost:
-            return start_index
-        else:
-            return -1
+        # If the total gas collected minus the total cost is non-negative, return start_index
+        return start_index if total_gas >= 0 else -1
