83c8ad27664b0bc40d15546d3ecdd03615e65110
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-23 19:40:46 +0100

    Time: 30 ms (84.98%) | Memory: 16.7 MB (5.51%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,19 +1,30 @@
+from collections import deque
+class Solution:
+    def decodeString(self, s: str) -> str:
 
-class Solution(object):
-    def decodeString(self, s):
-        stack = []; curNum = 0; curString = ''
-        for c in s:
-            if c == '[':
-                stack.append(curString)
-                stack.append(curNum)
-                curString = ''
-                curNum = 0
-            elif c == ']':
-                num = stack.pop()
-                prevString = stack.pop()
-                curString = prevString + num*curString
-            elif c.isdigit():   
-                curNum = curNum*10 + int(c)
+        strStack = deque()
+        tempStr=[]
+        for car in s:
+            print(strStack)
+            if car != "]":
+                strStack.append(car)
+                
             else:
-                curString += c
-        return curString
+            
+                while strStack[-1] != "[":
+                    tempStr.append(strStack.pop())
+                tempStr = tempStr[::-1]
+                print('tempStr',tempStr)
+
+                #pop [
+                strStack.pop()
+                k=[]
+                nums=["0","1","2","3","4","5","6","7","8","9"]
+                while strStack and strStack[-1] in nums:
+                    k.append(strStack.pop())
+                k= k[::-1]
+
+                k = int("".join(k))
+                strStack.append(k * "".join(tempStr))
+                tempStr =[]
+        return ''.join(strStack)
