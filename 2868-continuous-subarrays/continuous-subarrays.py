83f270a03fcbab1c04f97843c997be5b162eab19
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-23 09:35:08 +0100

    Time: 641 ms (59.07%) | Memory: 27.2 MB (29.54%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.py
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -1,13 +1,23 @@
 class Solution:
-    def continuousSubarrays(self, nums: List[int]) -> int:
-        i = res = 0
-        d = dict()
-        for j, num in enumerate(nums):
-            t = d.copy()
-            for k, v in t.items():
-                if abs(k - num) > 2:
-                    i = max(i, v + 1)
-                    d.pop(k)
-            d[num] = j
-            res += j - i + 1
-        return res
+    def continuousSubarrays(self, nums: List[int]) -> int:      
+        queue_max, queue_min = deque(), deque()
+        left, prev_max, prev_min, ans = 0, 0, 0, 0
+        for i, num in enumerate(nums):
+            while queue_max and nums[queue_max[0]] - num > 2:
+                prev_max = queue_max.popleft()
+            while queue_max and num > nums[queue_max[-1]]:
+                queue_max.pop()
+            queue_max.append(i)
+
+            while queue_min and num - nums[queue_min[0]] > 2:
+                prev_min = queue_min.popleft()    
+            while queue_min and num < nums[queue_min[-1]]:
+                queue_min.pop()
+            queue_min.append(i)
+        
+            if nums[prev_max] - num > 2:
+                left = queue_max[0]
+            elif num - nums[prev_min] > 2:
+                left = queue_min[0]
+            ans += i - left + 1      
+        return ans
