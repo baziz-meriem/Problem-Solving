0d63e87da820fc4248b4b3e065dc5acfb91ae1d8
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-25 11:35:45 +0100

    Time: 41 ms (41.49%) | Memory: 16.6 MB (25.89%) - LeetSync

--- 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.py
@@ -1,19 +1,15 @@
 class Solution:
-    def splitString(self, s):
-
-        def dfs(start, last):
-            res = False
-            for k in range(1, len(s)-last+1):
-                if (int(s[start:last])-int(s[last:last+k])) == 1:
-                    if dfs(last, last+k):
-                        return True
-                    res = True
-                else:
-                    res = False
-            return res
-
-        for i in range(1, len(s)):
-            if dfs(0, i):
+    def splitString(self, s: str) -> bool:
+        def dfs(index, prev):
+            if index == len(s):
                 return True
-                
+            for j in range(index, len(s)):
+                val = int(s[index: j+1])
+                if val+1 == prev and dfs(j+1, val):
+                    return True
+            return False
+            
+        for i in range(len(s) - 1):
+            val = int(s[:i +1])
+            if dfs(i +1, val): return True
         return False
