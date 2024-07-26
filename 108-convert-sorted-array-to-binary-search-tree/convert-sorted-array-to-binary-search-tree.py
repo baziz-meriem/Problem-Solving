f3a2ce00528f54462d7c0e08dbd6a1c002d89664
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-26 18:46:53 +0100

    Time: 57 ms (38.20%) | Memory: 17.8 MB (16.21%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -1,22 +1,16 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums):
-        if not nums:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if nums==[]:
             return None
-
-        # Find the middle element
-        mid = len(nums) // 2
-
-        # The middle element becomes the root
-        root = TreeNode(nums[mid])
-
-        # Recursively build the left and right subtrees
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
-
-        return root
+        else:
+            idx = len(nums)//2
+            node = TreeNode(nums[idx])
+            node.left = self.sortedArrayToBST(nums[: idx])
+            node.right = self.sortedArrayToBST(nums[idx+1:])
+            return node
