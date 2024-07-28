a38ff4e096242e342b1bf286e8a6e59ad10bfe5c
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-07-28 19:12:39 +0100

    Time: 665 ms (32.57%) | Memory: 52.3 MB (25.33%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,12 +1,15 @@
+from collections import Counter
 class Solution:
-      def findCenter(self, edges: List[List[int]]) -> int:
-        ad = defaultdict(list)
-        for i in edges:
-            ad[i[0]].append(i[1])
-            ad[i[1]].append(i[0])
-        x = len(ad)
-        print(x)
-        maxi = 0
-        for i,j in ad.items():
-            if len(j)==x-1:
-                return i
+     
+    def findCenter(self,edges):
+        # Flatten the edge list into a single list of nodes
+        flattened_nodes = [node for edge in edges for node in edge]
+
+        # Count occurrences of each node
+        node_counts = Counter(flattened_nodes)
+
+        # The center node will have a count of n-1 (where n is the number of nodes)
+        n = len(edges) + 1
+        for node, count in node_counts.items():
+            if count == n - 1:
+                return node
