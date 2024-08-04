1054d5c2f56b0870aeab88b43a8bb9a722f2b9e1
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-04 10:37:17 +0100

    Time: 289 ms (30.09%) | Memory: 18.1 MB (80.83%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,21 +1,24 @@
+import math
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        lo, hi, k = 1, max(piles), -1
-        
-        while lo <= hi:
-            mid = lo + ((hi - lo) >> 1)
+        current = max(piles)
+        low, high = 1, max(piles)+1
 
-            if self.numOfHours(mid, piles) <= h:
-                k = mid
-                hi = mid - 1
+        while low<=high:
+            mid = (high+low)//2
+            #print(mid)
+            hNeeded = 0
+            for num in piles:
+                hNeeded += math.ceil(num/ mid)
+                if hNeeded > h:
+                    break
+            #print("Needed",hNeeded)
+            
+            if hNeeded > h:
+                low = mid + 1
             else:
-                lo = mid + 1
-
-        return k
-
-    def numOfHours(self, k: int, piles: List[int]) -> int:
-        totalHours = 0
-        for pile in piles:
-            totalHours += math.ceil(pile / k)
-
-        return totalHours
+                high = mid - 1
+                current = min(current,mid)
+            #print('current is:',current)
+        return current
