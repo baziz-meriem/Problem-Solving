1d8cd8359b71d93b4af890381402808ce7b033b4
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-06 18:29:09 +0100

    Time: 35 ms (70.37%) | Memory: 16.2 MB (98.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -12,7 +12,6 @@ class Solution:
         prevFib = self.fib(n - 1, dic)
         prev2Fib = self.fib(n - 2, dic)
         
-        # Update the memoization dictionary
         dic[n] = prevFib + prev2Fib
         
         return dic[n]
