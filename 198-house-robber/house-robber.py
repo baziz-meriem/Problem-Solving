a9e028e4635f71fe744da836b388f09ac4c63f4e
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-06 19:58:45 +0100

    Time: 42 ms (16.57%) | Memory: 16.6 MB (24.90%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(index: int, memo: dict) -> int:
+            if index in memo:
+                return memo[index]
+            if index < 0:
+                return 0
+            if index == 0:
+                return nums[0]
+            if index == 1:
+                return max(nums[0], nums[1])
+            
+            rob_current = nums[index] + helper(index - 2, memo)
+            skip_current = helper(index - 1, memo)
+            memo[index] = max(rob_current, skip_current)
+            return memo[index]
+        
+        return helper(len(nums) - 1, {})
