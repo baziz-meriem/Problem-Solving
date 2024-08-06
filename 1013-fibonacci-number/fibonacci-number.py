efb018f32146f84e6d833218a95706ed32473030
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-06 18:28:44 +0100

    Time: 36 ms (66.87%) | Memory: 16.4 MB (88.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,18 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        if n==1:
-            return 1
-        elif n==0:
+    def fib(self, n: int, dic: dict = {}) -> int:
+
+        if n == 0:
             return 0
-        else:
-            return self.fib(n - 1)+ self.fib(n - 2)
+        elif n == 1:
+            return 1
+        
+        if n in dic:
+            return dic[n]
+        
+        prevFib = self.fib(n - 1, dic)
+        prev2Fib = self.fib(n - 2, dic)
+        
+        # Update the memoization dictionary
+        dic[n] = prevFib + prev2Fib
+        
+        return dic[n]
