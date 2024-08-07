b9c7bfe01671ecee6b9b8a7aceaf8f3802a21102
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-07 09:37:09 +0100

    Time: 45 ms (42.24%) | Memory: 16.8 MB (80.44%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,5 +1,17 @@
 import heapq
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        heapq.heapify(nums)
-        return nums[0]
+        min_val = float('inf')
+        high = len(nums)-1
+        low = 0
+        while low<=high:
+            mid = (high+low)//2
+           
+            if nums[mid] >= nums[low]:
+                min_val = min(min_val,nums[low])
+                low = mid+1
+
+            else:
+                min_val = min(min_val,nums[mid])
+                high = mid -1
+        return min_val
