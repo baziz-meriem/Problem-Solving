11cb864a6db25a3b2cc5ec2085eecbba9b3b2198
Meriem Baziz <74902350+baziz-meriem@users.noreply.github.com>
2024-08-09 17:38:01 +0100

    Time: 1013 ms (9.27%) | Memory: 54.8 MB (58.79%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,23 +1,12 @@
 class Solution:
-    def dfs(self, vertex, first=False):
-        if vertex in self.reachable:
-            return
-        if not first:
-            self.reachable.add(vertex)
-            first = False
-        for _vertex in self.adj_matrix[vertex]:
-            self.dfs(_vertex)
-
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        self.adj_matrix = defaultdict(list)
-        self.reachable = set()
-        for edge in edges:
-            self.adj_matrix[edge[0]].append(edge[1])
-        
-        all_nodes = set()
-        for i in range(n):
-            all_nodes.add(i)
-            self.dfs(i, True)
-        
-        return list(all_nodes - self.reachable)
-        
+        incoming = collections.defaultdict(list)
+        result  = []
+        for src,dest in edges:
+            incoming[dest].append(src)
+        print(incoming.items())
+
+        for edge in range(0,n):
+            if not incoming[edge]:
+                result.append(edge)
+        return result
